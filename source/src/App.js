29e885dd83c0a273e77e9baa593e46080e94eaf7
Karapuchka <karapu4ka2@gmail.com>
2023-04-16 19:36:04 +0300

    .

---
+++ source/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className='wrapper'>
+        Hello
+    </div>
+  )
+}
+
+export default App;
