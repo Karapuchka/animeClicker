53f5f4881b8d4924cc83afa683acee65629a7a89
Karapuchka <karapu4ka2@gmail.com>
2023-05-02 19:19:52 +0300

    .

--- source/src/App.js
+++ source/src/App.js
@@ -2,15 +2,12 @@ import { useEffect, useState } from 'react';
 
 import detect   from './resources/script/detect.js';
 
-import Main      from './components/main/Main.js';
-
 import './App.scss';
 
 function App() {
 
   return (
     <div className='wrapper'>
-        <Main />
     </div>
 )
 }
