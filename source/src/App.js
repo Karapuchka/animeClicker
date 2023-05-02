1a5396362a4754d9fb071fa186306db80dce3711
Karapuchka <karapu4ka2@gmail.com>
2023-05-02 14:44:42 +0300

    .

--- source/src/App.js
+++ source/src/App.js
@@ -1,25 +1,16 @@
 import { useEffect, useState } from 'react';
 
 import detect   from './resources/script/detect.js';
-import userDate from './resources/script/storageDate.js'
 
 import Main      from './components/main/Main.js';
-import Cursor from './components/cursor/Cursor.js';
 
 import './App.scss';
 
 function App() {
 
-  const [cursor, setCursor] = useState('default');
-
-  const changeCursor = (value)=>{
-      setCursor(cursor => value)
-  }
-
   return (
     <div className='wrapper'>
         <Main />
-        <Cursor person={cursor}/>
     </div>
 )
 }
