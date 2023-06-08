d23a35dc024109b831d3d0c9ec012e5fb4ef892f
Karapuchka <karapu4ka2@gmail.com>
2023-06-08 22:37:56 +0300

    Добавил новые иконки

--- source/src/components/achievPanel/AchievPanel.js
+++ source/src/components/achievPanel/AchievPanel.js
@@ -9,32 +9,18 @@ function AchievPanel({hero, score}){
 
     const [lifePanel, setLifePanel] = useState(true);
 
-    const [arrayLinksAchiev, setArrayLinksAchiev] = useState([]);
+    const [arrayLinksAchiev, setArrayLinksAchiev] = useState([['100', true], ['300', true], ['500', true], ['800', true], ['1000', true]]);
 
     useEffect(()=>{
-
-        if(arrayLinksAchiev.length == 0){
-
-            let newArray = arrayLinksAchiev;
-
-            for (const key in countClick.achievements[hero]) {
-
-                if (countClick.achievements[hero].hasOwnProperty.call(countClick.achievements[hero], key)) {
-
-                    newArray.push(countClick.achievements[hero][key])
-                }
-            }
-
-            setArrayLinksAchiev(newArray);
-        }
+       
     })
-
+    
     return(
         <AnimatePresence>{
             lifePanel && (
                 <motion.div className="hero-achiev">
                     <ul className="hero-achiev__list">
-                        {arrayLinksAchiev.map(item => <ItemAchiev hero={hero} score={score} blockAchiev={true} link={item} />)}
+                        {arrayLinksAchiev.map(item => <ItemAchiev key={hero.toString() + item[0].toString()} id={`${hero.toString()}-${item[0].toString()}`} score={score} blockAchiev={item[1]} />)}
                     </ul>
                 </motion.div>
             )}
@@ -42,13 +28,17 @@ function AchievPanel({hero, score}){
     )
 }
 
-function ItemAchiev({hero, score, blockAchiev, link}){
+function ItemAchiev({id, score, blockAchiev}){
 
     const [lifeItemAchiev, setLifeItemAchiev] = useState(true);
 
     const [animateDoorLoock, setAnimateDoorLoock] = useState({rotate: [0], scale: 1});
 
-    const [transitionDoorLoock, setTransitionDoorLoock] = useState({duration: .2})
+    const [transitionDoorLoock, setTransitionDoorLoock] = useState({duration: .2});
+
+    const [animatePlayBtn, setAnimatePlayBtn] = useState({scale: 1})
+
+    const [transitionPlayBtn, setTransitionPlayBtn] = useState({duration: .2});
 
     const onScaleDoorLoock = ()=> {
         if(animateDoorLoock.scale == 1){
@@ -68,6 +58,16 @@ function ItemAchiev({hero, score, blockAchiev, link}){
         }
     }
 
+    const onScalePlayBtn = ()=>{
+        if(animatePlayBtn.scale == 1){
+            setAnimatePlayBtn({scale: [1, .8, 1]})
+            setTransitionPlayBtn({duration: 0.5, time:[0, .5, 1]})
+        } else {
+            setAnimatePlayBtn({scale: 1})
+            setTransitionPlayBtn({duration: 0.5})
+        }
+    }
+
     if(blockAchiev){
 
         return(
@@ -75,11 +75,11 @@ function ItemAchiev({hero, score, blockAchiev, link}){
                 lifeItemAchiev && (
                     <motion.li className="hero-achiev__list__item">
 
-                        <div onMouseOver={() => onScaleDoorLoock()} onMouseOut={() => onScaleDoorLoock()} onMouseDown={()=> onRotateDoorLoock()} onMouseUp={()=> onRotateDoorLoock()} className="hero-achiev__list__item__block-panel">
-                            <motion.span whileHover={{scale: 1.3}} animate={animateDoorLoock} transition={transitionDoorLoock} className="hero-achiev__list__item__block-panel__door-loock"></motion.span>
+                        <div onMouseOver={() => onScaleDoorLoock()} onMouseOut={() => onScaleDoorLoock()} onMouseDown={()=> onRotateDoorLoock()} onMouseUp={()=> onRotateDoorLoock()} className="hero-achiev__list__item__use-panel">
+                            <motion.span whileHover={{scale: 1.3}} animate={animateDoorLoock} transition={transitionDoorLoock} className="hero-achiev__list__item__use-panel__door-loock"></motion.span>
                         </div>
 
-                        <iframe width={150} height={150} src={link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <div id={id} className="hero-achiev__list__item__images"></div>
                     
                     </motion.li>
                 )}
@@ -91,8 +91,12 @@ function ItemAchiev({hero, score, blockAchiev, link}){
             <AnimatePresence>{
                 lifeItemAchiev && (
                     <motion.li className="hero-achiev__list__item">
-                        
-                        <iframe width={150} height={150} src={link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+                        <div onMouseDown={() => onScalePlayBtn()} onMouseUp={()=> onScalePlayBtn()} className="hero-achiev__list__item__use-panel">
+                            <motion.span whileTap={{scale: [1, .8, 1]}} animate={animatePlayBtn} transition={transitionPlayBtn} className="hero-achiev__list__item__use-panel__play"></motion.span>
+                        </div>
+
+                        <div id={id} className="hero-achiev__list__item__images"></div>
                     
                     </motion.li>
                 )}
